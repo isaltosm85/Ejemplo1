47faf1de5ec346e91b0154fc30808c054da96a1b
Ivan Saltos <ivan_saltos_medina@hotmail.com>
2017-04-19 20:25:34 -0500

    Cambio 2025

--- Ejemplo/Ejemplo/urls.py
+++ Ejemplo/Ejemplo/urls.py
@@ -12,11 +12,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-    hola
+    hola q tal
 """
 from django.conf.urls import url
 from django.contrib import admin
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
